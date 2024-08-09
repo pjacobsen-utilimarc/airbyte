f223c18dc61435f192bf4e13da52e1914c309d7d
Pete Jacobsen <pjacobsen@utilimarc.com>
2024-08-09 16:59:55 -0500

    Update test_source.py

--- airbyte-integrations/connectors/source-gitlab/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-gitlab/unit_tests/test_source.py
@@ -13,7 +13,7 @@ from source_gitlab import SourceGitlab
 def test_streams(config):
     source = SourceGitlab()
     streams = source.streams(config)
-    assert len(streams) == 23
+    assert len(streams) == 25
     assert all([isinstance(stream, DeclarativeStream) for stream in streams])
 
 
